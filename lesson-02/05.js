efd8fbde6e6621bd2ef327116b96995fbbc95c9f
mike10 <mike10@ukr.net>
2023-09-03 12:16:04 +0200

    test 02-5 correct

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,9 +1,9 @@
-let passport = {
+const passport = {
     name: "Petr",
     surname: "Petrov",
 };
 
-let passport2 = { ...passport }
+const passport2 = { ...passport }
 passport2.name = 'Ivan'
 
 console.log(passport)
