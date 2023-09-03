df61f07acc873242e8862d9e06051f5b55326d67
mike10 <mike10@ukr.net>
2023-09-03 15:18:25 +0200

    test 03-1 correction

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,7 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter(item == "черный" || item == "красный" || item == "желтый")
+    return colors.filter(item => item == "черный" || item == "красный" || item == "желтый").join('-')
 }
 
 console.log(createColorString());
