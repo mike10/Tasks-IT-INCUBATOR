4afa7697a0a7e54adc1437613c1fd35c7c8f7de1
mike10 <mike10@ukr.net>
2023-09-03 12:29:26 +0200

    test 02-7 correction

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,7 +7,7 @@ let passportMarried = {
     }
 };
 
-let passportMarried2 = { ...passportMarried, address: { passportMarried.address } }
+let passportMarried2 = { ...passportMarried, address: { ...passportMarried.address } }
 passportMarried2.married = true
 
 console.log(passportMarried)
