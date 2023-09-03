6c2970884be854edd05680731aa130dda5f34fcd
mike10 <mike10@ukr.net>
2023-09-03 15:04:11 +0200

    test 03-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,7 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter()
+    return colors.filter(item == "черный" || item == "красный" || item == "желтый")
 }
 
 console.log(createColorString());
