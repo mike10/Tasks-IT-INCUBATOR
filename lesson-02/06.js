1c1da828ff6662c2a9bd4f28ad0d5d2e8dd25929
mike10 <mike10@ukr.net>
2023-09-03 12:20:55 +0200

    test 02-6

---
+++ lesson-02/06.js
@@ -0,0 +1,14 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let passportWithAddress2 = { ...passportWithAddress, address: { ...passportWithAddress.address } }
+passportWithAddress2.address.city = 'Bobryisk'
+
+console.log(passportWithAddress)
+console.log(passportWithAddress2)
