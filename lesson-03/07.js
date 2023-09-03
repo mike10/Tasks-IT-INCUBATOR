eba0f9ce3ef2ccf8a167c5e882b21ab9fe62c908
mike10 <mike10@ukr.net>
2023-09-03 16:10:33 +0200

    test 03-7

---
+++ lesson-03/07.js
@@ -0,0 +1,9 @@
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+
+numbers.map(item => item.filter(item2 => item2 % 2 == 0)
+
+console.log(numbers)
